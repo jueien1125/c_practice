bf35e56f1080c0568db896b5642dae0d12222935
rean <rean1125@gmail.com>
2017-07-30 21:15:41 +0800

    test2

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
+	
+	char *str = (char *) malloc(100);
+	strcpy(str,"hello");
+	free(str);
+	if(str!=NULL){
+		strcpy(str,"world");
+		printf(str);
+	}
 
 	system("pause");
 	return 0;
 }
+
 
